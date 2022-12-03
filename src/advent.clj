20623cd8652b906e69fba323ffd3e65fed6cf708
breggles <tilman.bregler@red-gate.com>
2022-12-03 20:09:10 +0000

    day 3

--- src/advent.clj
+++ src/advent.clj
@@ -2,7 +2,7 @@
 
 ; Day 1
 
-  (def input "11334
+  (defonce input "11334
 6264
 9318
 
@@ -2286,7 +2286,7 @@
 
 ; Day 2
 
-  (def input-day2 "B Z
+  (defonce input-day2 "B Z
 A X
 B Z
 B Z
@@ -4787,34 +4787,360 @@ A X
 A X
 C Y")
 
-  (defn dist [a b]
-    (mod (- a (dec b)) 3))
+(defn dist [a b]
+  (mod (- a (dec b)) 3))
 
-  (defn char->num [c base]
-    (- (int c) (int base)))
+(defn char->num [c base]
+  (- (int c) (int base)))
 
-  (defn chars->nums [[[c1] [c2]]]
-    [(char->num c1 \A)
-     (char->num c2 \X)])
+(defn chars->nums [[[c1] [c2]]]
+  [(char->num c1 \A)
+    (char->num c2 \X)])
 
-  (defn round-points-part-i [[n1 n2]]
-    (+ (* 3 (dist n2 n1))
-       (inc n2)))
+(defn round-points-part-i [[n1 n2]]
+  (+ (* 3 (dist n2 n1))
+      (inc n2)))
 
-  (defn round-points-part-ii [[n1 n2]]
-    (+ (* 3 n2)
-       (inc (mod (+ n1 (dec n2)) 3))))
+(defn round-points-part-ii [[n1 n2]]
+  (+ (* 3 n2)
+      (inc (mod (+ n1 (dec n2)) 3))))
 
-  (defn points [round-points]
-    (->> (clojure.string/split input-day2 #"\n")
-         (map #(clojure.string/split % #" "))
-         (map chars->nums)
-         (map round-points)
-         (apply +)))
+(defn points [round-points]
+  (->> (clojure.string/split input-day2 #"\n")
+        (map #(clojure.string/split % #" "))
+        (map chars->nums)
+        (map round-points)
+        (apply +)))
 
 (comment
 
   (points round-points-part-i)
 
   (points round-points-part-ii)
+
 )
+
+; Day 3
+
+(defonce input-day3 "BzRmmzZHzVBzgVQmZLPtqqffPqWqJmPLlL
+hpvvTDcrCjhpcrvcGGhfLHMlLtMCqflNlWPJlJ
+hGjhncHhGnhbTHczBBZVVSbRwgSgRV
+rWVQjPQQjGRWNSrWrPjcptwBpqqJBtJBBcZgMdtq
+zzmmpzfTCFpTlMlJJwBgMlqMBt
+TvLszpbhhTLmsnRQPDQGWDWRvQSR
+zGzvLlGlQHLGBQZlhBWhdjRdmdWRcjPj
+fTJNfTfNSRWPhjdjfj
+pbsbVVnpSnbVTprnbqqrzvLLgQlGLPLHll
+ZCCCsWvNvmsCsCmZLZmgMLRpQMhwQRpQRfphfprpTfpM
+tlncPjzlndctbzcPPBcjwDphwrfGGDffbDRpDTGG
+cdqnddwzqjNVWVLZZLZq
+DTLbDbRrlQbwhhNrmmfwdt
+zzMJMzjCjJJjvLjMPJpcgPpzfhHdfqWcqddwtwfqdttcNtdN
+pJCzVpCvDZBLsVRQ
+STzBBbJzRRBZBRTqCCsfZLtNNLClCsfh
+jsQnnQjjHcvQFrcPwCmtLCNlvDfftfff
+sGFscMQQMMpqzqbMbd
+QlNDWGsjQjgQllWQsbtzqTJczTJcbFmmFJJP
+MhLrhgLVndRmzJFzVqqJqP
+pSLnMdwhwdRZRSwhLZwLhdGWQjlsgWjNQWWSvgBsWDlj
+THjSRFSddTjdBTcPLcVVvVBw
+GzWnWfndWfznDfsnsBsPVwVwPPLL
+zNflzJWqqzQDdSStHNZNpFFtbj
+FSzDmsFSFlDlBzqVjqHHjHHpVgHLbp
+rTrTtTQQntRQnQJQgggHZttVgHLBLhZL
+WTJJRRQCRRJTRdBCRdvRNDFSWFMPmDlPPSsNPSzS
+WQldlMtMVQgVMQHnDGbHGGnRnQmD
+rqcZPrCFjmHlbGjZ
+zSScchqwchBzTzFzhhSlcCwNtdVWWJgsVdMtWNgNVWTJTd
+lMZqjMWllrTTspjprWWSSwgWNSVNDmWGVwFwgN
+cdCCdLHcnndHJnmCRntLBnRzDvFNtNDVzgSgwDgFNVzFVv
+BRLcCCJCmJdcRhfjPPZphrlrPqlZ
+GdGqcrrZGDrvDJJqJHcBvmFFgmFMMgMgBtMLTssLmF
+NbPVPDlljPmTmsTj
+VfQDhflCCRWdcrQwJvvnJv
+RLcWgLCqqPQLcqZwzHgwmmrmmtgwTw
+DhbhNrMpnJSDJwVTHmmTVnTTVj
+lrsvblMDlcWcfQPQ
+PVldlphHwGwJJGdjZZWsRbbsGsNWrWQbNbQR
+SqcDvTmDLtfmSmtqppfqzTgTQBrRQsbCFWbNNFQFBBrRbLNb
+MgtmTgtfpqVlnVddZMwV
+BdmfmPBPSbSNdGSdvWrwcZrccZPPcZnH
+jzzLsjsMRlQQVHwswvvZrCHrrT
+VqhzVFzplFlpLwpMphLRQQVRmSqgbdGtNJBmNSmgGbtggSgt
+DHVpNZjdZjFZWVFHpvFvzmlRzPnlfznFRz
+lrTBTsBwwMbrrwLPPfwGmGzvRf
+scrtMhMCtJBBBclbHdHttWZWDSqDSjHj
+wzqsPmqsbsfqBwPMNRMMZcZmFFNtZM
+CgCnhlvvLJgcRFNNBdCpWM
+QnQrVgHSvVHjbjTGBbbTHb
+HdrVrdqFDdZVmHgRmDRFHMnTdTssMGnLnPJLbPTbCs
+SczlScjwcNzplNzQSSfjwQSrTGGsbTsnTCnGTMCMLMGGbN
+wQlfjrhfhQFHqZhRZRtD
+RsfJDGJvzPNcjpddSWJWMd
+LLCbBCwCrCmVVnrmhQFmbVhdcdlWpjZzSpMdWSpcWczSBj
+rrLCbTwnHTvzvNGT
+wPhPhbCqqSCrtJDlqvlrJr
+RVVZddLFRZZcQLvJJtzptlgPJp
+TVQRZGVncFdTGWZdCNShHhfPNwwsWPwb
+dzLVzPSgrgDDDCMSMLLPwFmdTTcsvmwNwjNsHcFF
+nWBGntQfGNGBflWBBqlpRQGbWFvjwsbsFswbvTHjjbmHTc
+tBNJBnGBflQnDPJrPhDgrPVg
+VtWztWtqpqzWpWzqjNRjNpWTmrrmrSbnmJwSJwnMPrCSJVwM
+sDHsBDhBdsBZGcHvLHDLhhCSnRSwCJMZrPbmnMbJSCSR
+ccLRhgsLBdRsdHNTFFNNgqTglqzF
+hztlmDhPhgPlPNNgmZMCbmwwQjcwjjwMjVCd
+RSJRrRqnqQJFqvnTGrHCcHHCCHHbHHMcMvdM
+qGJsnQTRsStsftPlhPNl
+BFFBLPRCwsLwhlPlRmhcGGrbmmGjfNTTnp
+VJMVpzgqggJnrjmjNcMjmT
+VqdSZtQgZvtdzqHqHtVZdVQpCDWWFdwlRPDpWPPBCswlWD
+fCWCsjPzcbzwRSzVTzhhDLqvdg
+TmJtrNJrBLSLJqgS
+ptNTQFHrZlnpFPwsWMbRjCpcjR
+nJmQNCmbmlllmbClbfMLjMFqbGBsdLFq
+ZcgTWcTnMqqMTBqF
+tPgctSnPctZZgDWzZgQHwNmHlhlmzlQhlJlw
+ZpTCwpffdslvgShCBhqhRz
+FDMPnNFNmBPzvRPRBg
+nNgMrnnDGjDmJMmnFdZTTsdsrZrslcwcQr
+pTmczpCldcdDDnPttpvWSqbpJf
+jgjRZMGHhGLgQrjvPWzPJgJvzStbbq
+LGNLLNBBzcDFCBwwFC
+nJTTqnrNvTzNMzzNfqrTPrJnwpwPpZpsHccZVsBRpcVHwpcp
+bgDhgbghLWmFmStctVpZtBCVCCpfZp
+LLSgLGSjggFGbSSbmMnrvqvzjfzTNrJrqM
+RRpDmmPMTjwfGmJQgQ
+WsNscdnvvdVZFVnnrZbjjflwljlbzfGFjQjq
+NnLZsNnrrVVVcvdBLTPCPCRMwhPMBMPhCt
+lbVvzngGJnVbJHpHtHNPpdSQvc
+TsMBswFZsWMWBZMNwPtNNtRNHcNpSQ
+CcZCTrZDsjZTsTsshWhrWrTnfgbLDfJzVVLVVlgfnzfVGV
+JzTTRtJRZWmWjrMHCT
+DDFGlLGcGlSSSLsFGBspPBmNMBHMghmWNmWjWCmWtH
+nSSpnbsGlLDnpPsSSspFtVvffRQdVzqvvbqdfVQwRz
+sMhzszlHHDsWbthHDqsbJjpLNtmjVJmVLLVLVLBp
+nrTPrGwfPLdprzJzdL
+wgPQcTGGzgccwCgnRwgRChFhlWSDqWWQMWhssSsMQl
+NSNmwtpSpCpvMphCsr
+PHcRGPLJMrsvzsqG
+QHjbnRMcfbPbQZmlZgZlgBBQ
+cPRPbhQjbQRdtPQdLqLHqzFZjCFCqLjC
+mmfsnnwrfvwrfSNZFzHHLDCFNlLlqDlN
+wsmrwswwGTffMrBnmQttJtcMZQQtPJPbZc
+MvBPDDRRdnnvHPCHZLHZsFLL
+rmJcbVqbcjWwWjQHLzTZFTHSzFrpsz
+cmwllVqqGJbVVVmmqbQcmgRnRvGhGfgDRDZBBBvRdd
+nMvMhMnvhnbTZWSSZgHmGJDFmmNDzBmbNmdGBN
+rCsPLRCssRjrLLsrLlwRVrcNJQfDQfdBmmfNBGJNzmDPfB
+CRjCpLltgtJgJJWq
+jshCzJpjzTPpmCWvSlpfwHfSWglf
+LQMMNMnHtDtLVRvwwgRWlldgWD
+qHVrQNHVMFQtrrBBQMBcrrZsZbzCZhbbJZJsmmsmFPTC
+JZQZnsQNMqTngZqJBVfBfPPVBNrwvfPw
+SSmDstFjpDpCszDjcLLhrPVlGlrGGVBwrvwVPt
+FSssFcLjFjbmFFCzjLcFLRDnMJnTHRnZZTdWqZZWnMnRnZ
+GbHRHpldwGMpWhHpCMBlCbRdVSLhnqJLSrDPLPPLPDqVDrhh
+gvjWWQvgZFtQFFNqLnVnDnSJzzztDD
+ZccccfTsffHdWWdRWwsw
+ClCtbHMlnnPPlszV
+gSDWSLgWQWQJJNWqgtQjPsnfcdVcLVdVdzfzVzff
+WQgqtFQgDgQSFqJhqhSJvNDRrZMZHwHMCbZhTpZbGHMTMG
+pZJZlCQtHFhPfdNfCh
+zcmLSVczwcMcLDNFHdLPhPWH
+szvVVnBmnTGQtHTQ
+RVVCNDlNGzlGZqHGHWqWhGqQwH
+ZFLFTmpLvvmSqsbb
+TrfpBfJpJMlnnNfNZD
+qHHlDClHhltMqQsHDhHslGznwdTnzzwDGSdfnwGnwG
+mZRNcNcLLPNPBFFbbPmLmbZFSCVfJJTVndVfSwnRzznfTwCS
+CcCWFbbBLCWtgWgHjghqvv
+TjbzlnlFmfqCFFVVCRWr
+PhMcLpPDtMLpwPDvLPJbMhSgVCGqggVqQgCqCgCgSWvv
+btbZbNZhJDJJhDtwtsTTTmBzzBBmlNlmHj
+FqhjWtqlqmmsnFPTCvMCQMTTCjQd
+pfffRfLpgrgGgzrNVzzpGVzRCdMCPJbwwcVMbQPCJVMVdbww
+DGGDZRGrHggzSsFQnnWShmtH
+vtHVVMMrvVMVrSHvLgvlHcZFCnRCZcccZtRRZfJFCJ
+rdDjGsdTQDcNZfdncCRR
+rBDsTwBbjbmbbQswswPhqVmmSvpVhlvvqMhHhh
+vGBLrqMNvqSLBvvrNbllLHfwStWWtFttccjtRtjtcj
+MhCDJmhMDzmcRRcjzWfztH
+ZQDmDhVVCQbBVdVNMvvv
+ptCtCzhWPWptnhVzzpGZbZTjTjVjFGjVFgVl
+fQswRRffmRqZlgrqqFjjSgGg
+HwsQDNNsDsmRLLHmffsfvHptBnhtzCvhWpZWBdhnMdCh
+RlHzzTqczBPfbnvcpB
+wVtNwpSZstppwwMsZhsdnLvnbtBBmbnLFFdnmF
+WNQJMVWsZWwGJWhhSNrQzlgHrDCgQRHpCHrl
+RrZWpJZRrZpdTGstlchLGGlLMd
+NqjDPCQPnQCSvtMzSLhhjM
+nQVQDDDDfwBwNCVCNVFNpWpgJgrRTmLTmTmgRTWF
+SHMcrMHpcjGcjSrMMbvSvvSvwFTLJwJNtFGFWJNtDLFTLfWN
+zqRnPfzQCRzqsmRPzznhszzLtLwQwwFTgWWLDLgWFTwTNQ
+qVPZmRZhsCZPhZlRCqRRRCbfpccMBjvMVjdHjjMjSvdf
+VVQdHwBZLVltlddtBczhrzvGcWWFRwgsFG
+TDTTTqqTSSqjqnmTmPqPPmTmGhRszvsrzsjRsccgzrRzgWGF
+DpJPqpWqHbZpllpt
+cCSCFsnnZFnscDtNdJFJtJtdmb
+VgBqBsqRrHtNdzmNrt
+BGLLVVjRBsqPBfsGwPsMfSSZCSfTZTZQpSphfS
+plCHCHlgglHHGpNbtngNrDvBDpfQDBQfZDfWZVrr
+mTmMLhRfwhsLPQvQZDMZQBQWMB
+cwsssmqRTFFfFgtbCtGl
+LQPPrCPnMZwqtRMn
+cWTSlJWlcplJdDTdGdpDlGcGgqmtwwZtqRrNRRmRdNZqmgNq
+GSJcJSjsjTpsvWGWBHLLvVVBBBrFrzVz
+NVPCSPMNDSNFVSWCsJJJmpGmZZGLLcpZLHGGtsHt
+fwzlBBqghqvzqqlDrHbpHjZHmGZbLZrHLb
+dnBgnDqQvwRnSnnFMFMP
+BCbPsFFwCRHmDSBmWnvDDj
+phhZVzdpVfQZphhZpRhSVnjmrcvvnrWtDrvWDS
+TfQJMfLphMhJdfdzpQJRTPbwHHNlgbGwsTGgCP
+ttWLlnnvnNnBBtlTqWlpvpndQdZsQQFssFDdsRFdVdRNFQ
+jSgrScrbGZSGrrCGsFVMssFsPPFcDDMV
+bzSmJbfCZCbzLwllflwqtvvw
+zmFTJwFLPmzLztmjDzTJwfNrdFNrFppBSNRGNGdbrpBR
+gqlhWQgsZMsvqMlMMvsvqsNlLbcdppbrRpdbbcSrrbbr
+vssCgVgCsggZQZCgsnsqWgWvfJPDLwffwTPPmzTnjTPmPmwJ
+SpcRTPQLBLWpNNzjmmwwwRrR
+tGlfvGhfnbDlbqlChnfFMrwsmwNssTMHMHjFwv
+ZlhtCtffCdWcZWZVVT
+jTTCcWHWJNgCGTzTmnzrmnGn
+BwRRbFvtvvQmJJFMpMJr
+ZBBwLvqbBZsRsbVsZSqbcZdJjHHjhfPCJfJfHhgc
+VrnDSvvrLrfTdTLGfdRp
+zcJzmcFcHGfdGmWTVd
+tHsMhwPVctccHFHFcbSDbbPjnNbBnbvBQB
+QttWQwLTnLnWTtnffnLQSBFVjNvBjBFNgMdCsVWsjv
+pDqcmmRPHqgVBddjvN
+DcclzbcbPbJLnNTfnw
+plRcpsZDGlGZvWvMCNcLtttq
+SrfrwSjSVrSjwbmSrHzmHJCQQPQzqttNNQJMzJtqMW
+wSHVnfHfWwwHWFVfSnfgmmRsslFZZDBBGZsZsDTdGRTp
+qSFQSgQNgQBrBHHcrW
+VTmjVJLTwlTmwTVmsMJMVlJmPvcbvvbCBbGBPjGvBbBGWcbb
+DnJTZwmnZRhnpqNdWt
+dTVHjZLLZDVCfVHtLDDjQbscjWbSJMJPjsbWWb
+FnqrnmzzFllmsWwtsFtQMMFc
+lmqzzzngGmlNNBqGllzlBNRvptHHpTCHpDLpgDZdgvHvDD
+sdRZQbCfZTSTdlfTZCffccWPHPPcPPwLwctRnLWn
+BBJDzFVgCDrCJrqDJJhqJVVMLPHwcctFwcWHHGLcwGwGHnWc
+ghpJgqqjCZbQdZpd
+tbcpzbHSszcHBgqHGZgJJJhhww
+jfvdvRTffQQrrFCRFTnGwJRqNRZVpJGZLZggLh
+nQTjTnMndlTdQFMvnrClCnpzmzDtbbmBbcPSzzlmmtzP
+BqBqTCSTcqHsJHHM
+WWPGVPLtzVgWtjWPGzVjzVGcbDhPsRbDcsbJwNRswRDRss
+VQfWjfLFGWLjdFfVzTZZpJTpnmlTrSQlBl
+jLNsZjqSHCsGdsmpsm
+MvnVFzWMwMVWzfnVDwfBMfnnrCtdtPmPlRrdrJCJrtPDrrPD
+zznfFWwMfMfFMwVTMQFnQjhjgjSZhCNbLSTcHHgbbC
+GGtssttVmvnnGNMQrrVzgwVrCWMz
+FdhfhhcCDhHLfzclZMcrwcQMZM
+HHqqCBhHSSpdmjGqmGjtjtjj
+bbQLtGMQQtQRQtrDtGprrrbCqwplZhhqSqmdwvdzqqqhSmpS
+FsJjJBfnsJcFcFfjVPjWBzldqhqnlZZZzzhmnSvSnm
+JPcFfFWjFHJVVsVjPVscsDlLNRHGDbLRMRCDNrCGbG
+JdMdlMRJnTwdvcjv
+CDLHbNSzzLFgHvnTjrswBNBTNT
+QgbvzSFQmZQPQQRW
+NTBrNzrpjjjCwGbB
+FRbQlcvFvcRQQlRsMlRRRZjwCqMwjmjwJZdLJmjCZC
+cVPPQcvlWDNhrbPz
+VdbVtbbZJdtJVVdDVZmTLqqTSQvNLjjDShhvSG
+zplpnBnFpnrrlghGNpLNqHvqvjNj
+cWncllnlPFWzcMwtWWtsVLVRmJWCds
+ShLSTnZnTSttTSbLQdfSZTMwcDHwwcHnJvDHnlnlclMM
+NmPMsssRrVwjDclHJwwR
+gNNMWGzNmqGdtfZTbGGb
+sWNNlRHnmJtmntJt
+brbbBTbbFbCbqqGgBTrCfmQVVZfSSQQSVtJZSrVZ
+bbFqvbDvvGGLGbCCtBGDLbLlcPNHhhccPNcdPPchlsdR
+DCFvDvnCnNfMBmMMslDZML
+SQQQJHwpSgJSJHQWSWHqJWWbmcBBBLLTsmhhTcZbMhmlshcb
+RJRgpJHssgwSQHRqsQPGGjjtNCrrFvvnFjjPrP
+mThmsgjzTPjMpcvtWP
+GNNBVqVGNZbbNbNqqZQVNVNbWcpdtMCcpCtMWCdCPpQccmpp
+VSmNrmmbBfZVlsrssrLTRhRhTn
+TdmCvLDCpTRNTdFbbWnnSWCfhjbbzn
+GrrMsPVGcQHBGMbhjjSgWfHHDbjb
+BPBVqqrQPsQqwrrmmmJdRLDDqFRplT
+fpDDJljDlCfDTjprjrfbddWthCSCtdPPQFhSSSWW
+HsLZgMGbgBBsNzMvGbdVtVQzFRQSthhFPdtP
+sMBmGBmbNvLHGMnrDppTcJmcjpqljf
+ptSpSJQqpbNGGDDhcMWrlNHcZZWWls
+zRLRRRjvvgjHMMsMpWpc
+vmCPLCgwvwdnCzmvLbpTbVQqJJPbJPpTVq
+TJCfhhJVFffrJJQQllNWcvWhwvWD
+GPSGjjpLslBbpLpLqqqPDvdwvwvNzQWGzDDNdzGN
+msbRjbpPqsRpHnlZrmJlnVHT
+GGfFsCCTvGDsfTTrhsCMMzptZJMdpdgtrpdMcV
+LBlwBHPSqjwwlVggHpnMZcVHMt
+ZlZZlBbRPGGTGfmRsD
+CtCjbVvzQQZTWVdd
+MlSqWlmsmGBSHJHTDFHZ
+pcqsmsplwsqclwRtRWgtRnPPvb
+zCrzCrsdjrhGDCFqGDjRRPtpWfQQcpfQZcCZPp
+VSVwVMgLHHLTwMDTMMVnbWPRZQRcRQPptWnpbZcb
+MNBBBlSMvLVwTlVTFdNdhNhFsqsGDrzm
+rBLWTwTThWwVVDTwHBsZZWppvpGtpptppmRvFFFMFMfL
+qPPNCCbqcbcNqbqQjjJQqzjRpptmlpMGmMlJtftmtFHpMt
+QnCgzzQbbQqPcPQnncbdQdnVTwDssZgrShBTVgZZsBSDHT
+PFGJFqnfqmPgFJQPWdbLdpDRhbphWjDm
+rclNHvcrzCNwrWRprjdMMMph
+wsZHwZNvRRQsQqBV
+LqlGCPlPLTCPqqQlpqLlWfBfWgcHNRJRfWNsncGH
+VVtdwVtDDdVmhrdwSBmjbdzNHgfgJnNnsSnHsNffHgRsgR
+wVzhbjmDbDrwjdbztFDDthMCvqPppZQBQLZQTqTvFTvZ
+BnQnQFwRmRwmwdBSFDFnmSDVLCJTCTppVVmGLVTCLcgVpC
+ZlWvhvZjNrbNvqjNhlfPfqjCGHrsspggTpVLpsJCpcJVgg
+vPzNvqjWhqFzGSnRGMDG
+wZnMZzzZZchDRtVsqtCtwV
+WmWpWWmPPWrmrmBmWrTlTFPNVqVCRSDCQHcqVTtTqsSDSTSD
+PrppdFlWWlfrWmpWFffrdcGjJJGggnnhZGdLLgGGndvz
+FShHNmNhRhNJmBnQBQJrmP
+VTgzDTjwfffwzDvwlcczzVSJbQlBQSWBWCnPJPbJWWbC
+tzSVtzvSvGSRZqqFMNtpRR
+hPZhGDZpnCGtDhznjmLmdJffdNzJ
+glwsSrQwBvLdgLzdcj
+QsRbHllzzlHwHlBszWlTBFbpDPMhbPDVGpGFpPtFPp
+SRjStRDctgDSBzLvPvNrDhmPLr
+QqTHGTPJmmHmhNmH
+TGQZsTqFnQZCJTPsnJnZQMjVRBVtcVRSVRBlwccSCtBS
+bbsNsvsvnNPTRRllbblLqhtQCqQSLCGGHSqHNC
+wFpzFgqVzqVJWFDwqJDmSBBmHBHhShLQhCGSBCGH
+MJVpFMqgwMqRRbZsMbZMrP
+PPdDhvNDQdmgQPZmQVHHtHGGWVGbffWGvs
+MMLCTRRLlLclTLRMRLCwMLHWVctbVVHWWWFfVjVGsFWW
+MRSMMlpTJRqClBCRqBDnzqgQPnqgznZPZqbP
+MrMNPNNpjvdprWtrpMsthqBfqlnfqcGhVBqFRcnqFG
+QbDgSSQbgSDDmDVmlqSCRllRcFqnqfBl
+QVJbVmwwDQbzVTgbppNJNMWNjNNPrdpM
+WwJJNbtHfpLpVgZZPVFhZh
+vmmqlDvRvRfqBSrlzmmMjRBhcVhQVZhVghCQQQQTcTrPTP
+jSqMmqRzMDDjvqlBqsBMBmmwGNJwJnwLNfbGwddswnJtJH
+RLgRmRggbvbzzPmmRNmzsQWFtSGNtwSNQnntFwnnCw
+pDBrBHpHhlldphHBHhJVFSLnWWFJttCtQSttSS
+hfHrpphHBppfTvmzgMmbLbgf")
+
+(defn priority [c]
+  (let [ascii (int c)]
+    (if (> ascii (int \Z))
+      (inc  (- ascii (int \a)))
+      (+ 27 (- ascii (int \A))))))
+
+(->> (clojure.string/split input-day3 #"\n")
+     (map #(split-at (/ (count %) 2) %))
+     (map #(map set %))
+     (map #(apply clojure.set/intersection %))
+     (map first)
+     (map priority)
+     (apply +))
+
+(->> (clojure.string/split input-day3 #"\n")
+     (map set)
+     (partition 3)
+     (map (partial apply clojure.set/intersection))
+     (map first)
+     (map priority)
+     (apply +))
