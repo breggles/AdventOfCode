a68de327a8aec190a1f6e5e7f4eb17e507e7059e
breggles <tilman.bregler@red-gate.com>
2022-12-05 10:59:06 +0000

    day 4

--- src/advent.clj
+++ src/advent.clj
@@ -1,17 +1,19 @@
 (ns advent
+  (:require [clojure.string :as string]
+            [clojure.set :as st])
   (:use [data]))
 
 ; Day 1
 
 (comment
 
-  (->> (clojure.string/split input-day1 #"\n\n")
-       (map #(clojure.string/split % #"\n"))
+  (->> (string/split input-day1 #"\n\n")
+       (map #(string/split % #"\n"))
        (map #(apply + (map (fn [x] (Integer/parseInt x)) %)))
        (apply max))
 
-  (->> (clojure.string/split input #"\n\n")
-       (map #(clojure.string/split % #"\n"))
+  (->> (string/split input #"\n\n")
+       (map #(string/split % #"\n"))
        (map #(apply + (map (fn [x] (Integer/parseInt x)) %)))
        (sort)
        (reverse)
@@ -40,8 +42,8 @@
       (inc (mod (+ n1 (dec n2)) 3))))
 
 (defn points [round-points]
-  (->> (clojure.string/split input-day2 #"\n")
-        (map #(clojure.string/split % #" "))
+  (->> (string/split input-day2 #"\n")
+        (map #(string/split % #" "))
         (map chars->nums)
         (map round-points)
         (apply +)))
@@ -62,20 +64,53 @@
       (inc  (- ascii (int \a)))
       (+ 27 (- ascii (int \A))))))
 
-(->> (clojure.string/split input-day3 #"\n")
-     (map #(split-at (/ (count %) 2) %))
-     (map #(map set %))
-     (map #(apply clojure.set/intersection %))
-     (map first)
-     (map priority)
-     (apply +))
+(comment
 
-(->> (clojure.string/split input-day3 #"\n")
-     (map set)
-     (partition 3)
-     (map (partial apply clojure.set/intersection))
-     (map first)
-     (map priority)
-     (apply +))
+  (->> (string/split input-day3 #"\n")
+       (map #(split-at (/ (count %) 2) %))
+       (map #(map set %))
+       (map #(apply st/intersection %))
+       (map first)
+       (map priority)
+       (apply +))
+
+  (->> (string/split input-day3 #"\n")
+       (map set)
+       (partition 3)
+       (map (partial apply st/intersection))
+       (map first)
+       (map priority)
+       (apply +))
+
+)
 
 ; Day 4
+
+(defn range-contained? [[lb1 hb1 lb2 hb2]]
+  (or (<= lb1 lb2 hb2 hb1)
+      (<= lb2 lb1 hb1 hb2)))
+
+(defn ranges-overlap? [[lb1 hb1 lb2 hb2]]
+  (or (<= lb1 lb2 hb1)
+      (<= lb2 hb1 hb2)
+      (<= lb2 lb1 hb2)
+      (<= lb1 hb2 hb1)))
+
+(defn range-count [range-match?]
+  (->> (string/split input-day4 #"\n")
+       (map #(string/split % #","))
+       (flatten)
+       (map #(string/split % #"-"))
+       (flatten)
+       (map #(Integer/parseInt %))
+       (partition 4)
+       (filter range-match?)
+       (count)))
+
+(comment
+
+  (range-count range-contained?)
+
+  (range-count ranges-overlap?)
+
+)
